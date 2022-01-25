59e498188fb0dd1ab4297e56b9ef3d1572eff141
Ayanbola <olaoluwaayanbola@gmail.com>
2022-01-25 04:47:53 +0100

    fourth

--- index.js
+++ index.js
@@ -9,25 +9,10 @@ var api_key = "https://api.exchangerate-api.com/v4/latest/USD"
 var sectionONE = ToCurrency.options[ToCurrency.selectedIndex].value
 var sectionTwo = FromCurrency.options[FromCurrency.selectedIndex].value
 
-function Results() {
-    fetch(`${api}`)
-        .then(currency => {
-            return currency.json();
-        }).then(displayResults);
-}
-function displayResults(currency) {
-    let fromRate = currency.rates[sectionONE];
-    let toRate = currency.rates[sectionTwo];
-    btn.addEventListener("click",()=>{
-        finalValue.innerHTML = 
-        ((toRate / fromRate) * searchValue).toFixed(2);
-        open.style.display = "block"
-    })
-    finalValue.innerHTML = 
-       ((toRate / fromRate) * searchValue).toFixed(2);
-    finalAmount.style.display = "block";
-}
+
+
 btn.addEventListener("click",()=>{
+    displayResults()
     open.style.display = "block"
 })
 btn.addEventListener("dblclick",()=>{
